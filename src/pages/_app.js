674d21582c20eb7cd8149e3b1d6e516480ef9c70
btramel <bradtramel@gmail.com>
2025-06-25 11:06:17 -0500

    adds analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import { Analytics } from '@vercel/analytics/next'
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${GeistSans.className} ${GeistMono.className}`}>
+      <Analytics />
       <Component {...pageProps} />
     </main>
   )
